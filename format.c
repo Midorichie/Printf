50e4064ed24fce4efb3847686b3eb3ddb432e54b
Midorichie <hamsohood@gmail.com>
2023-11-12 15:08:50 +0100

    Format

--- format.c
+++ format.c
@@ -2,62 +2,67 @@
 /**
  * _printf - prints output in a format
  * @format: The format of the string
+ * @...: variable number of argument
  * Return: The number of characters printed, excluding null byte
  */
 int _printf(const char *format, ...)
 {
-	int total = 0;
+	int chara_count = 0;
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
 	va_start(args, format);
-
 	while (*format)
 	{
-		if (*format == '%' && *(format + 1) != '\0')
+		if (*format != '%')
 		{
-			switch (*(format + 1))
-			{
-				case 'c':
-					total += putchar(va_arg(args, int));
-					break;
-				case 's':
-					total += _printf(va_arg(args, char *));
-					break;
-				case '%':
-					total += putchar('%');
-					break;
-				default:
-					total += putchar('%') + putchar(*(format + 1));
-			}
-			format += 2;
+			write(1, format, 1);
+			chara_count++;
 		}
 		else
 		{
-			total += putchar(*format);
 			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				chara_count++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				chara_count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				chara_count += str_len;
+			}
 		}
+		format++;
 	}
 	va_end(args);
-	return (total);
+	return (chara_count);
 }
-
 /**
- * _printstr - Print a string
- * @str: String to print
+ * main - Entry point
  *
- * Return: Number of characters printed,
- * excluding null bytes
+ * Return: Always 0 (Success)
  */
-int _printstr(char *str)
+int main(void)
 {
-	int total = 0;
-
-	if (!str)
-		str = "(null)";
-	while (*str)
-	{
-		total += putchar(*str);
-		str++;
-	}
-	return (total);
+	_printf("Hammed\n");
+	_printf("%c\n", 'a');
+	_printf("%s\n", "String");
+	_printf("%%\n");
+	return (0);
 }
