082166b80158c39f0c3cb651b25994989e4358b4
Collatera24 <kola.tella24@gmail.com>
2023-11-14 15:55:44 -0800

    main.h file

--- main.h
+++ main.h
@@ -9,26 +9,20 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+char fmt;
+int (*fn)(va_list, char[], int, int, int, int);
+typedef struct fmt fmt_t;
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 int flags, int width, int precision, int size);
-int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+void print_buffer(char buffer[], int *buff_ind);
 
 
 #endif
