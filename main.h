934a63cf8255ef10551c46c2dede08669958039b
Midorichie <hamsohood@gmail.com>
2023-11-14 12:28:08 +0100

    Update

--- main.h
+++ main.h
@@ -7,20 +7,7 @@
 #include <unistd.h>
 #include <limits.h>
 
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} match;
-
 int _printf(const char *format, ...);
 int _putchar(char c);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _strlen(char *str);
-int _strlenc(const char *str);
-int print_37(void);
-int print_int(va_list args);
-int print_dec(va_list args);
 
 #endif
